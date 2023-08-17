df87ce954042d9176f681abe074359e61ac3d13d
AyaMouawi <ayamouawi8@gmail.com>
2023-08-17 11:17:27 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -25,4 +25,29 @@ console.log('lastName:', lastName);
 console.log('DOB:', DOB);
 
 
- 
+console.log(typeof "This is a text"); 
+console.log(typeof true); 
+console.log(typeof false); 
+console.log(typeof 1215); 
+console.log(typeof "999"); 
+console.log(typeof {}); 
+console.log(typeof []); 
+console.log(typeof null); 
+console.log(typeof undefined); 
+
+
+let vString = "Hello, world!";
+let vNumber = 42;
+let vBoolean = true;
+let vObject = { key: "value" };
+let vArray = [1, 2, 3];
+let vNull = null;
+
+
+console.log('Variables and Data Types:');
+console.log('vString:', vString, typeof vString);
+console.log('vNumber:', vNumber, typeof vNumber);
+console.log('vBoolean:', vBoolean, typeof vBoolean);
+console.log('vObject:', vObject, typeof vObject);
+console.log('vArray:', vArray, typeof vArray);
+console.log('vNull:', vNull, typeof vNull);
