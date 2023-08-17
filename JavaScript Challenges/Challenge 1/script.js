0b838babf3ad024a9d14e2132f25148fc2dba6f0
AyaMouawi <ayamouawi8@gmail.com>
2023-08-17 11:12:58 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -3,3 +3,26 @@ console.log('I am linked now!');
 var name;
 name = 'Aya Mouawi';
 alert('My name is ' + name);
+
+var age = 20;
+let lastName = 'Aya';
+const DOB = '12-7-2003';
+
+
+console.log('Initial values:');
+console.log('age:', age);
+console.log('lastName:', lastName);
+console.log('DOB:', DOB);
+
+
+age = 30;
+lastName = 'Not Mouawi';
+
+
+console.log('Updated values:');
+console.log('age:', age);
+console.log('lastName:', lastName);
+console.log('DOB:', DOB);
+
+
+ 
